0df725f11c9cc4a445398b186c3b8ed25d1d15a5
Wr2020 <comin_008@163.com>
2020-06-24 22:55:38 +0800

    Draw window

--- JFrameDemo/src/AlgoFrame.java
+++ JFrameDemo/src/AlgoFrame.java
@@ -1,2 +1,26 @@
-public class AlgoFrame {
+import lombok.Getter;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Getter
+public class AlgoFrame extends JFrame {
+
+    private static final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+    private int cavasWidth;
+    private int cavasHeight;
+
+    public AlgoFrame(String title, int cavasWidth, int cavasHeight) throws HeadlessException {
+        super(title);
+        this.cavasWidth = cavasWidth;
+        this.cavasHeight = cavasHeight;
+        setVisible(true);
+        setSize(cavasWidth, cavasHeight);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+
+    public AlgoFrame(String title) throws HeadlessException {
+        this(title, screen.width, screen.height);
+    }
 }
--- JFrameDemo/src/Main.java
+++ JFrameDemo/src/Main.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.awt.*;
 
 public class Main {
@@ -6,12 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         EventQueue.invokeLater(() -> {
-            JFrame frame = new JFrame("Welcome");
-            frame.setVisible(true);
-            frame.setSize(500, 500);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setResizable(true);
-
+            AlgoFrame frame = new AlgoFrame("Welcome");
         });
     }
 }
