26dda481939379e8f806a77c7be35526303d01d7
Wr2020 <comin_008@163.com>
2020-06-25 13:41:03 +0800

    抗锯齿，双缓存

--- JFrameDemo/src/AlgoFrame.java
+++ JFrameDemo/src/AlgoFrame.java
@@ -34,14 +34,23 @@ public class AlgoFrame extends JFrame {
 
     private class AlgoCanvas extends JPanel {
 
+        public AlgoCanvas() {
+            // 双缓存
+            super(true);
+        }
+
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
 
             Graphics2D g2d = (Graphics2D) g;
 
+            // 抗锯齿
+            RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+
             // 设置笔画宽度
-            AlgoVisHelper.setStrokeWidth(g2d, 5);
+            AlgoVisHelper.setStrokeWidth(g2d, 20);
 
             // 设置颜色
             AlgoVisHelper.setColor(g2d, Color.GREEN);
