7e29e2e6cfb15221b41f3ead8cb6105f53f289df
Wr2020 <comin_008@163.com>
2020-06-25 13:17:24 +0800

    绘制工具类

--- JFrameDemo/src/AlgoFrame.java
+++ JFrameDemo/src/AlgoFrame.java
@@ -15,13 +15,13 @@ public class AlgoFrame extends JFrame {
         super(title);
         this.cavasWidth = cavasWidth;
         this.cavasHeight = cavasHeight;
+        setSize(cavasWidth, cavasHeight);
         AlgoCanvas canvas = new AlgoCanvas();
         // 画布
         setContentPane(canvas);
         pack();
 
         setVisible(true);
-        setSize(cavasWidth, cavasHeight);
         // 关闭退出程序
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // resize
@@ -41,17 +41,16 @@ public class AlgoFrame extends JFrame {
             Graphics2D g2d = (Graphics2D) g;
 
             // 设置笔画宽度
-            int strkeWidth = 10;
-            g2d.setStroke(new BasicStroke(strkeWidth));
-            g2d.setColor(Color.RED);
+            AlgoVisHelper.setStrokeWidth(g2d, 5);
+
+            // 设置颜色
+            AlgoVisHelper.setColor(g2d, Color.GREEN);
             // 设置一个圆
-            Ellipse2D circle = new Ellipse2D.Double(50, 50, 300, 300);
+            AlgoVisHelper.fillCircle(g2d, cavasWidth / 2, cavasHeight / 2, 200);
 
-            g2d.draw(circle);
-
-            g2d.setColor(Color.GREEN);
-            circle = new Ellipse2D.Double(50, 50, 300, 300);
-            g2d.fill(circle);
+            // 第二个圆
+            AlgoVisHelper.setColor(g2d, Color.RED);
+            AlgoVisHelper.strokeCircle(g2d, cavasWidth / 2, cavasHeight / 2, 200);
         }
 
         @Override
