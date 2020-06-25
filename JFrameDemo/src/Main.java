be27bb5c2545cdc2d78d07e739f3f6830a81c9d0
Wr2020 <comin_008@163.com>
2020-06-25 16:34:31 +0800

    mvc重构

--- JFrameDemo/src/Main.java
+++ JFrameDemo/src/Main.java
@@ -8,29 +8,7 @@ public class Main {
         int sceneWidth = 800;
         int sceneHeight = 800;
         int N = 10;
-        int R = 50;
-        Circle[] circles = new Circle[N];
-        for (int i = 0; i < N; i++) {
-            int x = (int) (Math.random() * (sceneWidth - 2 * R)) + R;
-            int y = (int) (Math.random() * (sceneHeight - 2 * R)) + R;
-            int vx = (int) (Math.random() * 11) - 5;
-            int vy = (int) (Math.random() * 11) - 5;
-            circles[i] = new Circle(x, y, R, vx, vy);
-        }
 
-        EventQueue.invokeLater(() -> {
-            AlgoFrame frame = new AlgoFrame("Welcome", sceneWidth, sceneHeight);
-
-            new Thread(() -> {
-                for (; ; ) {
-                    // 绘制数据
-                    frame.render(circles);
-                    AlgoVisHelper.sleep(20);
-                    for (Circle circle : circles) {
-                        circle.move(0, 0, sceneWidth, sceneHeight);
-                    }
-                }
-            }).start();
-        });
+        AlgoVisualizer visualizer = new AlgoVisualizer(sceneWidth, sceneHeight, N);
     }
 }
